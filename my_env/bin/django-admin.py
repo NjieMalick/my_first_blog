ce54e3840a77b96d0a752ffd245d53c1520dd574
Malick <njognjiemalick@gmail.com>
2020-02-11 10:11:00 +0000

    My Django Girls app, first commit

---
+++ my_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/malick/Documents/django_projects/django_girls/my_env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
